b603dd1b7ef951158bc75cc554989b8acdbb47d4
Samuel-Njoroge <njorogekiragu8@gmail.com>
2026-08-31 23:43:25 +0300

    feat(source): add assets and CSV files

---
+++ src/daglearn/defs/assets.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import dagster as dg
+
+
+RAW_FILE = "src/daglearn/defs/data/raw.csv"
+PROCESSED_FILE = "src/daglearn/defs/data/processed.csv"
+
+
+@dg.asset
+def transform():
+
+    # Read data
+    df = pd.read_csv(RAW_FILE)
+
+    # Group sales by shipping city
+    df = (
+        df.groupby("shipping_city", as_index=False)["total_amount"]
+        .sum()
+        .rename(columns={"total_amount": "amount"})
+    )
+
+    # Save processed data
+    df.to_csv(PROCESSED_FILE, index=False)
+
+    return "Data aggregated successfully."
