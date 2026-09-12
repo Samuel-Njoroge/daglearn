07bb51dc001c0f40ac62136ff2657eaeec068c4d
Samuel-Njoroge <njorogekiragu8@gmail.com>
2026-09-12 23:49:54 +0300

    feat(assets): add a duckdb resource

--- src/daglearn/defs/assets.py
+++ src/daglearn/defs/assets.py
@@ -1,3 +1,5 @@
+from dagster_duckdb import DuckDBResource
+
 import pandas as pd
 import dagster as dg
 
@@ -23,3 +25,38 @@ def transform():
     df.to_csv(PROCESSED_FILE, index=False)
 
     return "Data aggregated successfully."
+
+
+@dg.asset
+def orders(duckdb: DuckDBResource):
+
+    file_path = "src/daglearn/defs/data/raw/orders.csv"
+    table_name = "orders"
+
+    with duckdb.get_connection() as conn:
+        conn.execute(
+            f"""
+            CREATE OR REPLACE TABLE {table_name} AS (
+            SELECT * FROM read_csv_auto('{file_path}')
+            )
+            """
+        )
+
+
+@dg.asset
+def customers(duckdb: DuckDBResource):
+
+    file_path = "src/daglearn/defs/data/raw/customers.csv"
+    table_name = "customers"
+
+    with duckdb.get_connection() as conn:
+        conn.execute(
+            f"""
+            CREATE OR REPLACE TABLE {table_name} AS (
+            SELECT * FROM read_csv_auto('{file_path}')
+            )
+            """
+        )
+
+
+
